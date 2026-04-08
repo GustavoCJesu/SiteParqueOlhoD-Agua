613cae8e2444ba4839a2a3b01dbdc2ea1b2b7a44
GustavoJesuino <gustavo.jesuino1129@gmail.com>
2026-04-08 16:50:39 -0300

    Pagina Principal Completa

---
+++ parque-olho-d-agua/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+
+import Image from  'next/image'
+import '../Header/header.module.css'
+import Link from 'next/link'
+
+
+
+export default function Header(){
+    return(
+        <header className='p-5'>
+            <ul className={`flex justify-around items-center header`}>
+                <li className='menuItem'><Link href='/'>Home</Link></li>
+                <li className='menuItem'><a href="#">Galeria</a></li>
+                <li className='menuItem'><a href="#">Serviços</a></li>
+                <li className=""><Image className='logoImg' src="/img/logo/LogoParque.png" alt="Logo parque olho d'agua" width={100} height={100}/></li>
+                <li className='menuItem'><a href="#">Blog</a></li>
+                <li className='menuItem'><a href="#">Contatenos</a></li>
+                <li className='menuItem'><Link href='/aboutUs'>Sobre Nós</Link></li>
+            </ul>
+        </header>
+    )
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+import flowbite from "flowbite-react/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
