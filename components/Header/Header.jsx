49bba45c7a0bd99a387c3639e291ad5cc53d0e8f
GustavoJesuino <gustavo.jesuino1129@gmail.com>
2026-04-29 13:13:23 -0300

    Correção Logo e Cor Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <header className='p-5 w-full text-black'>
+    <header className='p-5 w-full text-[#525252] bg-[#F8F9F5]'>
 
 
       <ul className='hidden md:flex justify-around items-center w-full text-xl'>
@@ -15,7 +15,7 @@ export default function Header() {
         <li><Link href='/galery'>Galeria</Link></li>
         <li><a href="/services">Serviços</a></li>
         <li>
-          <Image src="/img/logo/LogoParque.png" alt="Logo parque olho d'agua" width={100} height={100} />
+          <Image src="/img/logo/LogoNova.png"  alt="Logo parque olho d'agua" width={200} height={100} />
         </li>
         <li><a href="#">Blog</a></li>
         <li><a href="/contactUs">Contatenos</a></li>
@@ -24,7 +24,7 @@ export default function Header() {
 
       {/* Mobile */}
       <div className='flex md:hidden justify-between items-center w-full'>
-        <Image src="/img/logo/LogoParque.png" alt="Logo parque olho d'agua" width={70} height={70} />
+        <Image src="/img/logo/LogoNova.png" alt="Logo parque olho d'agua" width={150} height={100} />
         <button
           onPointerDown={() => setMenuOpen(!menuOpen)}
           className='text-3xl p-2 text-black cursor-pointer select-none'
